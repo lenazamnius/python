097b04f88517421c63729823d051049fc5e1bb2e
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-23 17:21:19 +0300

    test program

---
+++ converter.py
@@ -0,0 +1,4 @@
+question = input('How long did you sleep yesterday? ')
+minutes = int(question) * 60
+seconds = int(minutes) * 60
+print(f'You slept {question} hours that is {minutes} minutes or {seconds} seconds')
