5c569fc06340ecd708827f5553b77d633921a53a
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-24 19:03:16 +0300

    changed conditional statment

--- converter.py
+++ converter.py
@@ -2,9 +2,13 @@ print('How long did you sleep yesterday?')
 hours = int(input())
 minutes = hours * 60
 seconds = minutes * 60
-if hours > 9:
-    print(f'You slept {hours} hours that is {minutes} minutes or {seconds} seconds. Maybe you overslept.')
-elif hours < 6:
-    print(f'You slept {hours} hours that is {minutes} minutes or {seconds} seconds. Recommended to sleep more.')
+part = 24 / hours
+if hours:
+    if hours > 9:
+        print(f"You slept {hours} hours that is {minutes} minutes or {seconds} seconds. It's {part}th of the day. Maybe you overslept.")
+    elif hours < 6:
+        print(f"You slept {hours} hours that is {minutes} minutes or {seconds} seconds. It's {part}th of the day. Recommended to sleep more.")
+    else:
+        print(f"You slept {hours} hours that is {minutes} minutes or {seconds} seconds. It's {part}th of the day. Have a nice day)")
 else:
-    print(f'You slept {hours} hours that is {minutes} minutes or {seconds} seconds. Have a nice day)')
+    print('Enter whole number')
