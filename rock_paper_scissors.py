49d85dc875fe19dd4269f57bbd09d16e4df0c72d
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-25 12:34:33 +0300

    add more statements

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,18 +2,26 @@ print('...ROCK...\n...PAPER...\n...SCISSORS...')
 first_player = input('Make your choice. Rock, paper or scissors... ')
 second_player = input('Your turn. Make your choice. Rock, paper or scissors... ')
 
-if first_player == 'rock':
-    if second_player == 'paper':
-        print('Second_player win!!!')
+choice = ['rock', 'paper', 'scissors']
+
+if (first_player in choice) and (second_player in choice):
+    if first_player == second_player:
+        print("It's a tie!")
     else:
-        print('First_player win!!!')
-elif first_player == 'paper':
-    if second_player == 'rock':
-        print('First_player win!!!')
-    else:
-        print('Second_player win!!!')
-elif first_player == 'scissors':
-    if second_player == 'rock':
-        print('Second_player win!!!')
-    else:
-        print('First_player win!!!')
+        if first_player == choice[1]:
+            if second_player == choice[2]:
+                print('Second player win!!!')
+            else:
+                print('First player win!!!')
+        elif first_player == choice[2]:
+            if second_player == choice[1]:
+                print('First player win!!!')
+            else:
+                print('Second player win!!!')
+        elif first_player == choice[3]:
+            if second_player == choice[1]:
+                print('Second player win!!!')
+            else:
+                print('First player win!!!')
+else:
+    print('Chose only between rock, paper or scissors, please)')
