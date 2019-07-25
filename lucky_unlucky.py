d34bdbdd8174317ba92d961e42f304b17e9564b1
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-25 22:28:26 +0300

    add number sort with for_loop

---
+++ lucky_unlucky.py
@@ -0,0 +1,8 @@
+for num in range(1, 21):
+    if num == 4 or num == 13:
+        print(f'{num} is UNLUCKY!!!')
+    else:
+        if num % 2 == 0:
+            print(f'{num} is even')
+        else:
+            print(f'{num} is odd')
