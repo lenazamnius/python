65cb07e928ff9a91be5257f8193878596c45ae4c
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-25 16:47:40 +0300

    Add randomness

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,27 +1,30 @@
+import random
+
 print('...ROCK...\n...PAPER...\n...SCISSORS...')
 first_player = input('Make your choice. Rock, paper or scissors... ')
-second_player = input('Your turn. Make your choice. Rock, paper or scissors... ')
-
 choice = ['rock', 'paper', 'scissors']
+random_num = random.randint(0, 2)
+second_player = choice[random_num]
+print(f'Second player chose {second_player}')
 
 if (first_player in choice) and (second_player in choice):
     if first_player == second_player:
         print("It's a tie!")
     else:
-        if first_player == choice[1]:
-            if second_player == choice[2]:
+        if first_player == choice[0]:
+            if second_player == choice[1]:
                 print('Second player win!!!')
             else:
-                print('First player win!!!')
+                print('You win!!!')
+        elif first_player == choice[1]:
+            if second_player == choice[0]:
+                print('You win!!!')
+            else:
+                print('Second player win!!!')
         elif first_player == choice[2]:
-            if second_player == choice[1]:
-                print('First player win!!!')
-            else:
-                print('Second player win!!!')
-        elif first_player == choice[3]:
-            if second_player == choice[1]:
+            if second_player == choice[0]:
                 print('Second player win!!!')
             else:
-                print('First player win!!!')
+                print('You win!!!')
 else:
-    print('Chose only between rock, paper or scissors, please)')
+    print('Choose only between rock, paper or scissors, please)')
