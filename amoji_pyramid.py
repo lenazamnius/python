beb45029c3f7ae84e01beabd453334d5d616a55d
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-26 11:52:57 +0300

    add one more example while-loop

--- amoji_pyramid.py
+++ amoji_pyramid.py
@@ -1,24 +1,24 @@
-#print a pyramid
+# print a pyramid
 count = 1
 while count < 8:
-    print('*' * count)
-    count += 1
+    print('\U0001f600' * count)
+    count += 2
 print('\n')
 
-#print a pyramid up side down
+# print a pyramid up side down
 for num in range(8, 0, -1):
-    print('*' * num)
+    print('\U0001f600' * num)
 
 # print a dimond
 
 count = 1
-indent = 11
-while count < 12:
+indent = 12
+while count < 11:
     print((' ' * int(indent / 2)) + ('*' * count))
     count += 2
     indent -= 2
 
 indent = 2
-for num in range(9, 0, -2):
+for num in range(11, 0, -2):
     print((' ' * int(indent / 2)) + ('*' * num))
     indent += 2
---
+++ stop_coping_me.py
@@ -0,0 +1,4 @@
+str = input('Hey how are you?\n')
+while str != "stop copying me":
+    print(str.upper())
+    str = input()
