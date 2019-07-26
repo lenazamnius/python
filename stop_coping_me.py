7472556dc26b514d5d9179b8fe7a04070c4544ab
Lena Zamnius <lena.zamnius@gmail.com>
2019-07-26 12:06:59 +0300

    add break

--- stop_coping_me.py
+++ stop_coping_me.py
@@ -1,4 +1,12 @@
+# str = input('Hey how are you?\n')
+# while str != "stop copying me":
+#     print(str.upper())
+#     str = input()
+
+# with break
 str = input('Hey how are you?\n')
 while str != "stop copying me":
     print(str.upper())
     str = input()
+    if str == "SHUT UP!":
+        break
